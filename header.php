0a5f574ef246d85089d2dfe13f011ad16e98f12f
Diana Diabong <40291646+DianaDbg@users.noreply.github.com>
2021-02-02 13:13:14 +0000

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,6 @@ if(!isset($_SESSION))
   <script src="./style/bootstrap/js/bootstrap.js"></script> 
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <link href="https://cdn.quilljs.com/1.1.6/quill.snow.css" rel="stylesheet">
-  <!-- <script src="https://cdn.quilljs.com/1.1.6/quill.js"></script> -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
